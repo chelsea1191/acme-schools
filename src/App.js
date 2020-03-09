5fcd88fbb8206e603ba17265aeb8f7e1d5b44613
Chelsea Kramer <chelsea.kramer99@gmail.com>
2020-03-09 19:58:09 -0400

    boilerplate

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
-  //const [recipes, setRecipes] = useState([]);
+  //useState here
 
   const getChickenRecipes = async () => {
     const response = await fetch(
